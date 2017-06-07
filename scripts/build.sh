a2e3189902fc97fd9cc98d777c8151b77d2ece1f
Alex Beimler <alex-beimler@web.de>
2017-06-07 02:17:28 +0200

    add gitlab ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 cmake --build $BUILD_PATH --target creature_backend -- -j2
-if [ "${BUILD_ONLY}" != "1" ]; then cmake --build $BUILD_PATH --target creature_backend_build_test -- -j2; fi
-if [ "${BUILD_ONLY}" != "1" ]; then cmake --build $BUILD_PATH --target creature_backend_check; fi
-if [ "${COVERAGE}" = "1" ]; then bash <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/master/codecov) -x $GCOV -a "-s `pwd`" ; fi
--- scripts/travis.script.sh
+++ scripts/travis.script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker exec creature_${CONTAINER_NAME} /bin/bash ./scripts/build.sh
+docker exec creature_${CONTAINER_NAME} /bin/bash ./scripts/test.sh
